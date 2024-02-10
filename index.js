74d2643b13f5b2d4f997b4782df536729f801bc4
jaganck12 <jaganck12@gmail.com>
2024-02-10 16:39:59 +0530

    mediabackend

---
+++ index.js
@@ -0,0 +1,34 @@
+// import json-server in index.js file
+
+const jsonServer = require('json-server')
+
+
+//create a server for json file
+
+const mediaPlayerServer = jsonServer.create()
+
+
+//set up a path for json file
+
+const router = jsonServer.router('db.json')
+
+
+//return a middleware used by json server
+
+const middlewares = jsonServer.defaults()
+
+
+//setup a port for running json server
+
+const port = 4000 || process.env.port
+
+//use middleware and router in server
+
+mediaPlayerServer.use(middlewares)
+mediaPlayerServer.use(router)
+
+//to listen server for resolving request from server
+
+mediaPlayerServer.listen(port,()=>{
+          console.log(`mediaplayer listning at port ${port},and waiting from the client request !!!`)
+})
